e59fae5387b64983432c388879cebdeeb210e322
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2026-02-27 16:46:56 +0530

    updat

---
+++ Encapsulation.java
@@ -0,0 +1,46 @@
+public class Encapsulation {
+    
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.age = -10;   // Direct access
+        System.out.println(s.age);
+
+        Student_Enc s1 = new Student_Enc();
+        s1.setAge(-10);   // Cannot set invalid value
+    }
+}
+
+class Student {
+    public int age;
+}
+
+
+
+class Student_Enc {
+     // Private variables (data hiding)
+     private int age;
+     private String name;
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Public getter method
+    public String getName() {
+        return name;
+    }
+
+    // Public setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
